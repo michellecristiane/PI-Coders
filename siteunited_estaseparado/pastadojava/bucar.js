920b61b9481b43c2a9dfd6a29ba600e28092fb8e
Nadja <150749906+Nadja-Almeida@users.noreply.github.com>
2024-08-20 19:54:06 -0300

    site

---
+++ siteunited_estaseparado/pastadojava/bucar.js
@@ -0,0 +1,9 @@
+let boxBuscar = document.querySelector('buscar-box');
+
+let lupa = document.querySelector('lupa-buscar');
+
+let btnFechar = document.querySelector('btn-fechar');
+
+lupa.addEventListener('click', ()=>{
+    boxBuscar.classList.add('ativar')
+})
---
+++ telainicial_obs_ttemcodigomisturado/pastadojava/index.js
@@ -0,0 +1,53 @@
+let dropdowns = document.querySelectorAll('.navbar .dropdown-toggler')
+let dropdownIsOpen = false
+
+// Handle dropdown menues(Manipular menus suspensos)
+if (dropdowns.length) {
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', (event) => {
+      let target = document.querySelector(`#${event.target.dataset.dropdown}`)
+
+      if (target) {
+        if (target.classList.contains('show')) {
+          target.classList.remove('show')
+          dropdownIsOpen = false
+        } else {
+          target.classList.add('show')
+          dropdownIsOpen = true
+        }
+      }
+    })
+  })
+}
+
+// Handle closing dropdowns if a user clicked the body(Manipular menus suspensos de fechamento se um usuário clicou no corpo)
+window.addEventListener('mouseup', (event) => {
+  if (dropdownIsOpen) {
+    dropdowns.forEach((dropdownButton) => {
+      let dropdown = document.querySelector(`#${dropdownButton.dataset.dropdown}`)
+      let targetIsDropdown = dropdown == event.target
+
+      if (dropdownButton == event.target) {
+        return
+      }
+
+      if ((!targetIsDropdown) && (!dropdown.contains(event.target))) {
+        dropdown.classList.remove('show')
+      }
+    })
+  }
+})
+function handleSmallScreens() {
+  document.querySelector('.navbar-toggler')
+    .addEventListener('click', () => {
+      let navbarMenu = document.querySelector('.navbar-menu')
+
+      if (!navbarMenu.classList.contains('active')) {
+        navbarMenu.classList.add('active')
+      } else {
+        navbarMenu.classList.remove('active')
+      }
+    })
+}
+
+handleSmallScreens()
